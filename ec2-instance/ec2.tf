8bbc65adf1f2bfe7d768628fb7cc1cc4d0b87b79
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-06-23 07:40:49 +0530

    Terraform

---
+++ ec2-instance/ec2.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "database" {
+  ami = "ami-09c813fb71547fc4f"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0a0cf70a415b59000"
+  associate_public_ip_address = true
+  tags = {
+    Name = "DatabaseInstance"
+  }
+}
+
+resource "aws_security_group" "allow_all"{
+  name = "allow_all"
+  description = "Allow all inbound traffic  for testing"
+  
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+    ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "AllowAllSecurityGroup"
+  }
+}
