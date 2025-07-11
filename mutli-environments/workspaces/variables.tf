6690693d364b7881da16e2ee5d590fde8d21adbb
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-07-11 08:40:31 +0530

    Data sources

---
+++ mutli-environments/workspaces/variables.tf
@@ -0,0 +1,43 @@
+variable "common_tags" {
+  type = map
+  default = {
+    Project = "expense"
+    Environment = "dev"
+  }
+}
+
+
+variable "instances" {
+  default = ["mysql", "backend", "frontend"]
+}
+
+variable "zone_id" {
+  description = "The ID of the Route 53 hosted zone."
+  type        = string
+  default     = "Z04987052ECGSU780E7WV"
+  
+}
+
+variable "domain_name" {
+  description = "The domain name for the Route 53 record."
+  type        = string
+  default     = "sridevsecops.store"
+  
+}
+
+variable "project" {
+  default = "expense"
+}
+
+variable "instance_type" {
+  default = {
+    dev = "t3.micro"
+    qa = "t3.small"
+    staging = "t3.large"
+    prod = "t3.xlarge"
+  }
+  
+}
+
+variable "environment"{
+}
